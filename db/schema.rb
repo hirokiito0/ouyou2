9e4dd84a86224f5779388c3cc6bf808a0a7140a6
hirokiito0 <syu.itou.h@gmail.com>
2021-07-05 10:54:49 +0000

    [finish]kadai4_follow機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_100106) do
+ActiveRecord::Schema.define(version: 2021_07_05_062212) do
 
   create_table "book_comments", force: :cascade do |t|
     t.string "content"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_07_04_100106) do
     t.datetime "updated_at", null: false
     t.integer "book_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
